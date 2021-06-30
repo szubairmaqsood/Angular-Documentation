f9b0572d9284b9a71aaf631273d068eb65663233
zubair-pc\zubair <szubair@gmail.com>
2021-06-30 15:51:07 +0500

    Pipes are used to format data like date and currency

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -7,12 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
    index:number[] =[0,1,2];
-  
+   birthday:Date = new Date(1988, 3, 15);
+   amount:number=30;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggle = true; // start with true == shortDate
+
+  get format()   { return this.toggle ? 'shortDate' : 'fullDate'; }
+  toggleFormat() { this.toggle = !this.toggle; }
+  
   addItem(newItem:String) {
     this.index.push(Number(newItem));
   }
